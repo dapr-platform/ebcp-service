4daafd42a84297559f987293527b6ca124ea6064
lyl <liuyulong@defensedriver.com.cn>
2025-02-19 14:35:07 +0800

    修改：     api/init_route.go

--- api/init_route.go
+++ api/init_route.go
@@ -3,15 +3,14 @@ package api
 import "github.com/go-chi/chi/v5"
 
 func InitRoute(r chi.Router) {
-	InitEbcp_schedule_timeRoute(r)
 	InitEbcp_exhibition_areaRoute(r)
 	InitEbcp_exhibition_roomRoute(r)
 	InitEbcp_exhibition_itemRoute(r)
-	InitEbcp_deviceRoute(r)
 	InitEbcp_exhibition_area_detailsRoute(r)
 	InitEbcp_exhibition_hall_detailsRoute(r)
 	InitEbcp_exhibition_hallRoute(r)
-	InitEbcp_schedule_actionRoute(r)
-	InitEbcp_schedule_taskRoute(r)
-	InitEbcp_schedule_timeRoute(r)
+	InitEbcp_cameraRoute(r)
+	InitEbcp_playerRoute(r)
+	InitEbcp_item_device_relationRoute(r)
+	InitEbcp_item_scheduleRoute(r)
 }
